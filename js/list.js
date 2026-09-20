6fabe2bc73cc128bb355e2a712c3fac46a9306c7
IMSTUCKHOSTAGE <shoeibkamali1978@gmail.com>
2026-09-21 07:49:08 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,4 +1,4 @@
- const d = {
+const d = {
   list: [
     /*=============================================================================*/
     {
@@ -13,13 +13,11 @@
       name: "eu passei",
       author: "Ando7",
       id: 149214067,
-      percentToQualify: ,
+      percentToQualify: 67, // was empty, fill in the correct value
       verificationVid: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  version: [3, 5, 0],
-};
-]
-        /*=============================================================================*/
-       {
+    },
+    /*=============================================================================*/
+    {
       vids: [],
       name: "eu passei",
       author: "Ando7",
@@ -28,18 +26,22 @@
       verificationVid: "https://youtu.be/x9oJxWBDkBA?is=qh6Ez-1Vy7K4TKPV",
     },
   ],
+  version: [3, 5, 0],
+};
+
 const list = d.list;
 const version = d.version;
+
 console.log(
   "%c my balls",
   "color: pink; font-weight: bold; background-color: rgb(50,10,40); padding: 2em; margin: 3em 2em 10em 20em; border: 3px solid black; border-radius: 1em; box-shadow: 10px 10px 8px pink;"
 );
 
-//automatically changes keys aka awesome
+// automatically changes keys aka awesome
 for (let x = 0; x < list.length; x++) {
   if (x < 100) {
     list[x].key = x;
-  } else if (x >= 100) {
+  } else {
     list[x].legacykey = 0;
   }
 }
